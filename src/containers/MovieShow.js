a3ebd1138256d3cae27e160f4d88326f8f8f589d
theboymo <billfero@gmail.com>
2018-05-31 08:43:30 +0100

    Done.

--- src/containers/MovieShow.js
+++ src/containers/MovieShow.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const MovieShow = props =>
+const MovieShow = ({ movie }) =>
   <div>
-    <h3>Movie Show Component!</h3>
+    <h3>Title: { movie.title }</h3>
   </div>;
 
-export default MovieShow;
+// we can access any dynamic pieces of the URL via the ownProps.match.params obj
+// assuming we find a movie, add it to the props
+const mapStateToProps = (state, ownProps) => {
+  const movie = state.movies.find(movie => movie.id === ownProps.match.params.movieId);
+  if(movie) {
+    return { movie }
+  } else {
+    return { movie: {} }
+  }
+};
+
+export default connect(mapStateToProps)(MovieShow);
