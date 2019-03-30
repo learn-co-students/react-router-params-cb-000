47ed78ce5f04d83b42654eca87e0f33d8d888605
Felice Forby <forby.1@gmail.com>
2019-03-30 19:29:45 +0900

    add nested routes for movies and new movie action

--- src/containers/MovieShow.js
+++ src/containers/MovieShow.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 const MovieShow = props =>
   <div>
-    <h3>Movie Show Component!</h3>
+    <h3>Title: {props.movie.title}</h3>
   </div>;
+  
+const mapStateToProps = (state, ownProps) => {
+  const movie = state.movies.find(movie => movie.id === parseInt(ownProps.match.params.movieId));
+  if (movie) {
+    return { movie }
+  } else {
+    return { movie: {} }
+  }
+}
 
-export default MovieShow;
+export default connect(mapStateToProps)(MovieShow);
